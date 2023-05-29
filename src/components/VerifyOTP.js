448b5e724120c66a5ec11824dcc6e16aba2b2336
James Njenga <knjenga94@gmail.com>
2023-05-29 05:10:35 +0300

    Create verifyotp.css and styled VerifyOTP component

--- src/components/VerifyOTP.js
+++ src/components/VerifyOTP.js
@@ -1,16 +1,17 @@
-
+import './verifyotp.css'
 function VerifyOTP(){
     return(
         <div className="verify">
+            <h1>Verify your OTP code here</h1>
         <form>
-       
+       <div className='container'>
             <input
             type = "text"
             placeholder = "OTP"
             name ="otp"
             />
             <button type="submit">Verify OTP</button>
-    
+            </div>
         </form>
     </div>    
      
