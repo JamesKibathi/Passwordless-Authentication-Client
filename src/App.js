e753b55a8982ad2478f272b657e86d48d2039878
James Njenga <knjenga94@gmail.com>
2023-05-29 03:39:33 +0300

    Create Home page and add routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
-import Home from "./pages/Home"
+import Home from './pages/Home'
+import Login from './components/Login';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    
-    <Home/>
 
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+
+
     </div>
   );
 }
