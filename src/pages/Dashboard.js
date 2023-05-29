1839ba42bb1ac05bcae4db17239de09149f026c0
James Njenga <knjenga94@gmail.com>
2023-05-30 00:17:50 +0300

    Modify code to protect dashboard and logout functionality

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import "./dashboard.css"
-function Dashboard(){
-    return(
-        <div className="dashboard-container">
-       
-          <div className="welcome-message">
-            <h2>Welcome, James!</h2>
-            <button>Hide Welcome Message</button>
-          </div>
-        
-  
-        <button className="logout-button">Logout</button>
+function Dashboard() {
+  const navigate = useNavigate()
+  const token = localStorage.getItem("jwtToken")
+  console.log(token)
+  if (token) {
+    return (
+      <div className="dashboard-container">
+
+        <div className="welcome-message">
+          <h2>Welcome, James!</h2>
+          <button>Hide Welcome Message</button>
+        </div>
+
+
+        <button onClick={() => {
+          navigate("/")
+          localStorage.clear()
+        }} className="logout-button">Logout</button>
       </div>
     );
+  } else {
+    return <div>Unauthorized Access!!</div>
+  }
+
 }
 
 export default Dashboard
